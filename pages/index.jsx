710488dd2d5ecd3d5c840254b4494167dab50cb9
AdAntonio07 <antonio.ferreira1320@gmail.com>
2024-02-16 16:28:19 -0300

    Criado Componente post

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,28 @@ import styled from 'styled-components'
 import Navbar from '../src/componets/layout/Navbar'
 import Container from '../src/componets/layout/Container'
 import CreatePost from '../src/componets/cards/CreatePost'
+import H3 from '../src/componets/typography/H3'
+import Post from '../src/componets/cards/post'
 
 const Content = styled.div`
-margin: 50px 0;
+  margin: 50px 0;
+`
+const LastPostText = styled(H3)`
+  padding: 40px 0;
+`
+const RefreshPosts = styled.span`
+  font-weight: bold;
+  color: ${props => props.theme.primary};
+  cursor: pointer;
+`
+const RefreshPostsContainer = styled.div`
+  text-align: center;
+`
+const PostContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap: 20px;
+margin-top: 20px; 
 `
 
 function HomePage() {
@@ -16,8 +35,18 @@ function HomePage() {
       <Content>
         <Container>
           <CreatePost />
+          <LastPostText>Últimas postagens:</LastPostText>
+          <RefreshPostsContainer>
+            <RefreshPosts>Carregar novas postagens</RefreshPosts>
+          </RefreshPostsContainer>
+          <PostContainer>
+            <Post />
+            <Post />
+            <Post />
+          </PostContainer>
         </Container>
       </Content>
+
     </>
   )
 }
