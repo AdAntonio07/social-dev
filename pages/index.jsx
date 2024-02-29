69a16f1f5b31ee942510eb1e5ecf28788ac9fdc0
AdAntonio07 <antonio.ferreira1320@gmail.com>
2024-02-29 16:30:16 -0300

    Utilizado sessão no frontend e criado logout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components'
+import { withIronSessionSsr } from 'iron-session/next'
+
+import { ironConfig } from '../lib/middlewares/ironSession'
 
 import Navbar from '../src/componets/layout/Navbar'
 import Container from '../src/componets/layout/Container'
@@ -27,14 +30,14 @@ gap: 20px;
 margin-top: 20px; 
 `
 
-function HomePage() {
+function HomePage({ user }) {
 
   return (
     <>
       <Navbar />
       <Content>
         <Container>
-          <CreatePost />
+          <CreatePost username={user.user} />
           <LastPostText>Últimas postagens:</LastPostText>
           <RefreshPostsContainer>
             <RefreshPosts>Carregar novas postagens</RefreshPosts>
@@ -50,5 +53,27 @@ function HomePage() {
     </>
   )
 }
+
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps({ req }) {
+    const user = req.session.user
+
+    if (!user) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: '/login'
+        }
+      }
+    }
+
+    return {
+      props: {
+        user
+      }
+    }
+  },
+  ironConfig
+)
 
 export default HomePage
